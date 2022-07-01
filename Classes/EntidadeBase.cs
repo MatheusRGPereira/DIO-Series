c527110aaef801d4761cd30f46adb51136c7eb57
Matheus <matheusgaldinoinfo@gmail.com>
2022-07-01 14:06:30 -0300

    Subindo desafio de projeto que faz o crud de uma plataforma de series

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIOSeries.Classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
